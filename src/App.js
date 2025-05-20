a8a5bf1ba6cb1eda8f90b8a32f439e3af663252b
Windy <lt33562@gmail.com>
2025-05-20 17:36:20 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello from cô cảnh </h1>
+      <h1>Hello from cô thầy 22414ertert</h1>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders hello message', () => {
   render(<App />);
-  const headingElement = screen.getByText(/hello from cô cảnh/i);
+  const headingElement = screen.getByText(/hello from cô thầy/i);
   expect(headingElement).toBeInTheDocument();
 });
